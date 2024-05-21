d35de0121420fce1ecd20736ff95cf21ba9c0109
Dhanalakshmiparameswaran <dhanamsakthi44@gmail.com>
2024-05-21 12:28:00 +0530

    patch: recommit recent changes

--- src/routes/adminRoutes.ts
+++ src/routes/adminRoutes.ts
@@ -1,9 +1,15 @@
-import express from "express";
-const router = express.Router(); 
-import { get, post, del } from "../controllers/adminController"; 
+import { Router } from "express";
+import { deleteUB, updateUB ,showbook , addnewbook , deletebook} from "../controllers/adminController";
+const router = Router();
 
-router.post('/postBook', post);
-router.get('/show', get);
-router.delete('/deleteBook/:id', del); 
+router.post('/postBook', addnewbook );
+router.get('/show', showbook );
+router.delete('/deleteBook/:id', deletebook); 
 
-export const routes = router;
+//create router for admin to delete a UB data
+router.delete("/deleteUB", adminMiddleware, deleteUB);
+
+//create route for admin to update a UB data
+router.patch("/updateUB", adminMiddleware, updateUB);
+import
+export { router as adminRoutes };
