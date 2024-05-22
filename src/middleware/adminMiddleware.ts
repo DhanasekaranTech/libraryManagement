7a91737103366ef9854c5d75f2ddcc204dc4c785
Madhubalan-R <madhubalanbe016@gmail.com>
2024-05-22 09:54:19 +0530

    commit changes

--- src/middleware/adminMiddleware.ts
+++ src/middleware/adminMiddleware.ts
@@ -1,6 +1,6 @@
 import { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
-import { authMiddleware } from "./authMidd";
+
 export const adminMiddleware = (
   req: Request,
   res: Response,
